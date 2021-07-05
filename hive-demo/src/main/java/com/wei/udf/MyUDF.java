a5fd562b5c4e9b29b7ee8550a94ed08addd01330
scout <weikongliang@icloud.com>
2021-07-05 19:56:43 +0800

    first test

--- hive-demo/src/main/java/com/wei/udf/MyUDF.java
+++ hive-demo/src/main/java/com/wei/udf/MyUDF.java
@@ -4,16 +4,32 @@ import org.apache.hadoop.hive.ql.exec.UDFArgumentException;
 import org.apache.hadoop.hive.ql.metadata.HiveException;
 import org.apache.hadoop.hive.ql.udf.generic.GenericUDF;
 import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector;
+import org.apache.hadoop.hive.serde2.objectinspector.primitive.PrimitiveObjectInspectorFactory;
 
 public class MyUDF extends GenericUDF {
     @Override
     public ObjectInspector initialize(ObjectInspector[] objectInspectors) throws UDFArgumentException {
-        return null;
+
+        if (objectInspectors.length != 1) {
+            throw new UDFArgumentException("参数个数不为一");
+
+        }
+
+        return PrimitiveObjectInspectorFactory.javaIntObjectInspector;
     }
 
+    //处理数据
     @Override
     public Object evaluate(DeferredObject[] deferredObjects) throws HiveException {
-        return null;
+        //1.取出输入数据
+//        String input = deferredObjects.toString();
+        String input = deferredObjects[0].toString();
+        //2.判断输入数据是否为null
+        if (input == null) {
+            return 0;
+        }
+        //3.返回输入数据的长度
+        return input.length();
     }
 
     @Override
